c32a16338aa77938da68fdda4fd3f06b54e82f7f
Dylan Medina <medinabaez1120@gmail.com>
2024-07-10 15:36:42 -0500

    2024-07-10 15:36:42

--- hivik2/settings.py
+++ hivik2/settings.py
@@ -149,7 +149,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'auxiliarmto@serport.co'
+EMAIL_HOST_USER = 'analistamto@serport.co'
 EMAIL_HOST_PASSWORD = 'fund tewp sxap rbox'
 EMAIL_USE_SSL = False
 
