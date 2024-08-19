2f4e879435cb728773eefe1f11d514551bfc450a
Dylan Medina <medinabaez1120@gmail.com>
2024-08-19 18:10:10 -0500

    2024-08-19 18:10:10

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # Set the server details
 SERVER="ubuntu@got.serport.co"
 REMOTE_PATH="/home/ubuntu/got/django_hivik"
-LOCAL_PATH="/c/Users/medin/Desktop/django_hivik"
+LOCAL_PATH="/c/Users/medin/Desktop/Dylan/django_hivik"
 cd "$LOCAL_PATH"
 git add .
 git commit -m "$(date +"%Y-%m-%d %H:%M:%S")"
