519cd92016fb768de09e095104b0de4e4dc056d8
Dylan Medina <medinabaez1120@gmail.com>
2024-09-04 08:48:06 -0500

    2024-09-04 08:48:06

--- got/signals.py
+++ got/signals.py
@@ -3,6 +3,7 @@ from django.dispatch import receiver
 from .models import HistoryHour, TransaccionSuministro, DailyFuelConsumption, Equipo, Item
 from django.db.models import Avg, Sum, Min
 from decimal import Decimal
+from datetime import datetime
 
 @receiver(post_save, sender=HistoryHour)
 @receiver(post_delete, sender=HistoryHour)
@@ -90,7 +91,10 @@ def create_daily_fuel_consumption(sender, instance, **kwargs):
             suministro__item__id=132  # ID del combustible
         ).aggregate(primer_fecha=Min('fecha'))['primer_fecha']  
 
-        today = instance.fecha.date()
+        if isinstance(instance.fecha, str):
+            today = datetime.strptime(instance.fecha, '%Y-%m-%d').date()
+        else:
+            today = instance.fecha
         
         equipos = Equipo.objects.filter(system__asset=asset, tipo='r')
 
