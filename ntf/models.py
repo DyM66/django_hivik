477a6c4ae62f0853e6a46b54948e285a4ecda4b0
DyM66 <medinabaez1120@gmail.com>
2025-03-15 20:44:32 -0500

    2025-03-15 20:44:32

--- ntf/models.py
+++ ntf/models.py
@@ -4,13 +4,13 @@ from django.contrib.auth.models import User
 
 class WebPushSubscription(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="push_subscriptions")
-    endpoint = models.URLField()
+    endpoint = models.URLField(max_length=500) 
     p256dh = models.CharField(max_length=255)
     auth = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Push subscription for {self.user.username}"
+        return f"{self.user.username} - {self.endpoint[:30]}"
     
 
 class Notification(models.Model):
